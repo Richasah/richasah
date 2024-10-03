2094f3ce6c91b199e7c0c709a2a822cf23b6b463
Richasah <richasah1911@gmail.com>
2024-10-03 16:26:04 +0530

    Add files via upload

---
+++ projects script/calculater/calculater.js
@@ -0,0 +1,34 @@
+let currentValue='';
+document.querySelector('#work').value=currentValue;
+
+
+
+var background=document.getElementById('background')
+background.onclick=function(){
+
+      document.body.style.backgroundColor=(' rgb(226, 96, 113)')}
+      
+var background1=document.getElementById('background1')
+background1.onclick=function(){
+
+      document.body.style.backgroundColor=(' rgb(199, 253, 235)')}
+      
+var background2=document.getElementById('background2')
+background2.onclick=function(){
+
+      document.body.style.backgroundColor=(' rgb(252, 169, 201')}
+      
+var background3=document.getElementById('background3')
+background3.onclick=function(){
+
+      document.body.style.backgroundColor=(' rgb(201, 152, 247)')}
+      
+var background4=document.getElementById('background4')
+background4.onclick=function(){
+
+      document.body.style.backgroundColor=('rgb(238, 235, 235)')}
+                  
+var background5=document.getElementById('background5')
+background5.onclick=function(){
+
+      document.body.style.backgroundColor=('black')}
